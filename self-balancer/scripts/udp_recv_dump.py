f5744eb29383c5a93a779fcdb62553f1e81dbd66
Sahil Kale <sahil.kale1@gmail.com>
2024-07-21 17:20:22 -0700

    More stuffs

---
+++ self-balancer/scripts/udp_recv_dump.py
@@ -0,0 +1,32 @@
+import socket
+import argparse
+import time
+
+DEFAULT_SERVER_IP = "192.168.1.41"
+DEFAULT_SERVER_PORT = 5007
+
+if __name__ == '__main__':
+    # create args for the server IP and port
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--server_ip", type=str, default=DEFAULT_SERVER_IP)
+    parser.add_argument("--server_port", type=int, default=DEFAULT_SERVER_PORT)
+    args = parser.parse_args()
+
+    SERVER_IP = args.server_ip
+    SERVER_PORT = args.server_port
+
+    # Create a UDP socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # allow the socket to be reused (just in case weird restarts or something)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    # send a message to the server (poll the user first via stdin)
+    message = input("Enter a message to send to the server: ")
+    sock.sendto(message.encode(), (SERVER_IP, SERVER_PORT))
+
+    while True:
+        # receive a message from the server
+        data, addr = sock.recvfrom(1024)
+        # print the message alongside a timestamp accurate to us
+        print(f"Timestamp: {time.time()} | Received message from server: {data.decode()}")
+
