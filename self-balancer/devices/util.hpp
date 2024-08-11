941a26c6b33ff55dc42748cf809f0817825b824b
Sahil Kale <sahil.kale1@gmail.com>
2024-08-11 13:55:34 -0700

    More comm manager stuff

---
+++ self-balancer/devices/util.hpp
@@ -0,0 +1,31 @@
+#ifndef UTIL_HPP
+#define UTIL_HPP
+
+#include <stdint.h>
+
+// Function to swap endianness of uint16_t
+inline uint16_t swap_endianness(uint16_t value) {
+    return (value >> 8) | (value << 8);
+}
+
+// Function to swap endianness of uint32_t
+inline uint32_t swap_endianness(uint32_t value) {
+    return ((value >> 24) & 0x000000FF) |
+           ((value >>  8) & 0x0000FF00) |
+           ((value <<  8) & 0x00FF0000) |
+           ((value << 24) & 0xFF000000);
+}
+
+// Function to swap endianness of uint64_t
+inline uint64_t swap_endianness(uint64_t value) {
+    return ((value >> 56) & 0x00000000000000FF) |
+           ((value >> 40) & 0x000000000000FF00) |
+           ((value >> 24) & 0x0000000000FF0000) |
+           ((value >>  8) & 0x00000000FF000000) |
+           ((value <<  8) & 0x000000FF00000000) |
+           ((value << 24) & 0x0000FF0000000000) |
+           ((value << 40) & 0x00FF000000000000) |
+           ((value << 56) & 0xFF00000000000000);
+}
+
+#endif // UTIL_HPP
--- self-balancer/scripts/test_application.py
+++ self-balancer/scripts/test_application.py
@@ -1,14 +1,33 @@
 import os
+import argparse
 
-#chdir to "application"
-os.chdir("application")
+def main(debug):
+    #chdir to "application"
+    os.chdir("application")
 
-# make a build directory
-os.system("mkdir -p build")
-os.chdir("build")
+    # make a build directory
+    os.system("mkdir -p build")
+    os.chdir("build")
 
-# run "cmake ."
-cmd = r'cmake .. -DCMAKE_BUILD_TYPE=Debug -G "Unix Makefiles"'
-os.system(cmd)
-os.system("make all")
-os.system("./application_tests")
+    # run "cmake ."
+    cmd = r'cmake .. -DCMAKE_BUILD_TYPE=Debug -G "Unix Makefiles"'
+    os.system(cmd)
+    # make the application, check that the ret code is 0
+    ret = os.system("make all")
+    if ret != 0:
+        print("Error building the application")
+        exit(1)
+
+    if debug:
+        os.system("gdb ./application_tests")
+    else:
+        os.system("./application_tests")
+
+if __name__ == "__main__":
+    # create an arg parser for whether to debug or not
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--debug", help="Debug mode", action="store_true")
+    args = parser.parse_args()
+
+    main(args.debug)
+
