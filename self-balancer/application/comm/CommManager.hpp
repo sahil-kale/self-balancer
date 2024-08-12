812603f9b225a16940140eb2e74e8a6e56b691f3
Sahil Kale <sahil.kale1@gmail.com>
2024-08-11 22:22:39 -0700

    Fix message queue buffer init

--- self-balancer/application/comm/CommManager.hpp
+++ self-balancer/application/comm/CommManager.hpp
@@ -2,7 +2,7 @@
 #define COMM_MANAGER_HPP
 #include "TransportLayer.hpp"
 #include "MessageQueue.hpp"
-#define MAX_DATAGRAM_BUF_SIZE (512)
+#define MAX_DATAGRAM_BUF_SIZE (QUEUE_STORAGE_SIZE_BYTES)
 
 class CommManager {
     public:
--- self-balancer/devices/MessageQueue.hpp
+++ self-balancer/devices/MessageQueue.hpp
@@ -6,6 +6,9 @@
 #include "time.hpp"
 
 #define MAX_MESSAGE_BUF_BYTES 64
+#define MAX_MESSAGE_QUEUE_SIZE 50
+#define BYTES_PER_QUEUE_ITEM (sizeof(MessageQueue::Message))
+#define QUEUE_STORAGE_SIZE_BYTES (MAX_MESSAGE_QUEUE_SIZE * BYTES_PER_QUEUE_ITEM)
 
 class MessageQueue {
     public:
